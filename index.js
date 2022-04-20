7962afef19fe70b2207c9fd07320e20ae191f386
saket105 <saketcool35@gmail.com>
2022-04-21 01:11:19 +0530

    working with ejs

--- index.js
+++ index.js
@@ -7,15 +7,38 @@ app.use("/design", express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-// Post method
-app.post("/", (req, res) => {
-  const result = Number(req.body.num1) + Number(req.body.num2);
-  req.body.ans = result;
-  console.log(result);
-  res.send("Your num1 + num2 = " + result);
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+      break;
+  }
+
+  res.render("base", { day });
 });
 
 // Server running on port 4000

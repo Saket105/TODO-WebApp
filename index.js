bbd34cc88d1d37fa7f5c280b5404182e91862170
saket105 <saketcool35@gmail.com>
2022-04-21 20:19:51 +0530

    done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var items = [];
+let workItems = [];
 app.get("/", (req, res) => {
   var today = new Date();
   var options = {
@@ -21,8 +22,23 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("base", { day: "Work List", items: workItems });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 // Server running on port 4000

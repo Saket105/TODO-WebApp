6293dbbc201e1d5ab8bb226a0b9542e867bebf4c
saket105 <saketcool35@gmail.com>
2022-04-21 19:41:00 +0530

    it's working

--- index.js
+++ index.js
@@ -3,42 +3,26 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
-app.use("/design", express.static(__dirname));
+app.use( express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var items = [];
 app.get("/", (req, res) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-      break;
-  }
+  res.render("base", { day, items });
+});
 
-  res.render("base", { day });
+app.post("/", (req, res) => {
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 // Server running on port 4000
